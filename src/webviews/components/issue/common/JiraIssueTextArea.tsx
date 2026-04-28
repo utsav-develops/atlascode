861d173e348a0d0e2766fef5557f29e412d8a7fa
dazu <utsavacharya96@gmail.com>
2026-04-28 18:45:49 +0545

    border color fixed, reference to existing fields

--- src/webviews/components/issue/common/JiraIssueTextArea.tsx
+++ src/webviews/components/issue/common/JiraIssueTextArea.tsx
@@ -70,7 +70,7 @@ const JiraIssueTextAreaEditor: React.FC<Props> = ({
                     style={{
                         background: 'var(--vscode-input-background)',
                         color: isDisabled ? 'var(--vscode-disabledForeground)' : 'var(--vscode-input-foreground)',
-                        border: '1px solid var(--vscode-input-border)',
+                        border: '1px solid var(--vscode-settings-textInputBorder)',
                         caretColor: 'var(--vscode-editorCursor-background)',
                         minHeight: isDescription ? '175px' : '100px',
                         borderRadius: '2px',
